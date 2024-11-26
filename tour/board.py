2d90a887e050429237f34c3465141f565a245703
Kyle <kbburkholder@gmail.com>
2024-11-26 10:07:59 -0500

    make invalid move detection see if move has already been taken

--- tour/board.py
+++ tour/board.py
@@ -9,10 +9,11 @@ class Board:
         self.last_move = (starting_x, starting_y)
         self.moves = [*self.get_right_moves(), *self.get_left_moves(), *self.get_down_moves(), *self.get_up_moves()]
 
+    # My intent is that we just remove taken paths and once we are out of moves we know this path is not valid
     def get_next_move(self):
-        for move in self.moves:
-            if self.is_move_valid(move):
-                return move
+        self.remove_invalid_moves()
+        if len(self.moves) > 0:
+            return self.moves.pop(0)
         return (-1, -1)
 
     def remove_invalid_moves(self):
@@ -37,7 +38,11 @@ class Board:
         return ((self.last_move[0] - 1, self.last_move[1] - 2), (self.last_move[0] + 1, self.last_move[1] - 2))
 
     def is_move_valid(self, move):
-        return self.is_x_valid(move[0]) and self.is_y_valid(move[1]) and self.cells[move[0]][move[1]] == 0
+        move_is_on_board = self.is_x_valid(move[0]) and self.is_y_valid(move[1]) and self.cells[move[0]][move[1]] == 0
+        move_has_already_been_taken = False
+        if move_is_on_board:
+            move_has_already_been_taken = self.cells[move[0]][move[1]] == 0
+        return move_is_on_board and move_has_already_been_taken
 
     def is_x_valid(self, x):
         return x < self.board_size[0] and x >= 0
@@ -45,11 +50,14 @@ class Board:
     def is_y_valid(self, y):
         return y < self.board_size[1] and y >= 0
 
-    def get_next_move(self):
-        pass
-
     def is_path_blocked(self):
-        pass
+        if len(self.moves) == 0 and not self.is_tour_complete():
+            return True
+        return False
 
     def is_tour_complete(self):
-        pass
+        for column in self.cells:
+            for cell in column:
+                if cell == 0:
+                    return False
+        return True
